13c9122903078d954e098e62631be7f93108c60b
juliolmuller <juliolmuller@outlook.com>
2020-04-23 23:19:59 -0300

    Updated entry point files

--- src/dist.js
+++ src/dist.js
@@ -2,8 +2,4 @@
 /*
  * Entry point for browser version
  */
-window.xomSharePoint = require('./sharepoint/index')
-window.xomHttpFactory = require('./sharepoint/http/xomHttpFactory')
-window.XomSharePointSite = require('./sharepoint/objects/XomSharePointSite')
-window.XomSharePointList = require('./sharepoint/objects/XomSharePointList')
-window.XomSharePointSurvey = require('./sharepoint/objects/XomSharePointSurvey')
+window.xomSharePoint = require('./sharepoint')
