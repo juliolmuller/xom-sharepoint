1e21af1f82a0fecb7bd62de7b1c6db00892035e3
Julio L. Muller <julio.l.muller@exxonmobil.com>
2020-09-14 17:58:31 -0300

    Created methods overloading

--- src/XomSharePointList.ts
+++ src/XomSharePointList.ts
@@ -1,6 +1,7 @@
 import { XomApiClient, XomApiResponse, XomApiQueryString } from './@types'
 import genFileBuffer from '@lacussoft/to-arraybuffer'
 import * as requests from './facades/requests'
+import * as exceptions from './facades/exceptions'
 
 /**
  * Instantiate the object with the necessary information to connect to a SharePoint list through its REST API.
@@ -8,9 +9,7 @@ import * as requests from './facades/requests'
 class XomSharePointList {
 
   private _title: string
-
   private _http: XomApiClient
-
   private _itemsType: Promise<string>
 
   constructor(listTitle: string, httpInstance: XomApiClient) {
@@ -26,88 +25,145 @@ class XomSharePointList {
   /**
    * Returns the list fields metadata;
    */
-  public fields(params?: XomApiQueryString): Promise<XomApiResponse> {
+  public getFields(params?: XomApiQueryString): Promise<XomApiResponse> {
     return requests.getListFields(this._http, this._title, params)
   }
 
   /**
    * Returns a list of the items stored in the list.
    */
-  public get(params?: XomApiQueryString): Promise<XomApiResponse> {
+  public getItems(params?: XomApiQueryString): Promise<XomApiResponse> {
     return requests.getListItems(this._http, this._title, params)
   }
 
   /**
    * Returns a single list item with the given ID.
    */
-  public find(id: number, params?: XomApiQueryString): Promise<XomApiResponse> {
-    return requests.getListItemById(this._http, this._title, id, params)
+  public findItem(itemId: number, params?: XomApiQueryString): Promise<XomApiResponse> {
+    return requests.getListItemById(this._http, this._title, itemId, params)
+  }
+
+  /**
+   * Creates or updates a record based on an existing ID.
+   */
+  public saveItem(data: any): Promise<XomApiResponse>
+  public saveItem(itemId: number, data: any): Promise<XomApiResponse>
+  public saveItem(param1: any, param2?: any): Promise<XomApiResponse> {
+    const { Id: id, ...rest } = param2 || param1
+
+    return id
+      ? this.updateItem(id, rest)
+      : this.createItem(rest)
   }
 
   /**
    * Saves a new record in the SharePoint list.
    */
-  public async post(data: any): Promise<XomApiResponse> {
+  public async createItem(data: any): Promise<XomApiResponse> {
     return requests.postListItem(this._http, this._title, await this._itemsType, data)
   }
 
   /**
    * Updates an existing record in the SharePoint list.
    */
-  public async update(id: number, data: any): Promise<XomApiResponse> {
-    return requests.patchListItem(this._http, this._title, id, await this._itemsType, data)
-  }
+  public updateItem(item: any): Promise<XomApiResponse>
+  public updateItem(itemId: number, data: any): Promise<XomApiResponse>
+  public async updateItem(param1: any, param2?: any): Promise<XomApiResponse> {
+    const { Id: id = param1, ...rest } = param2 || param1
 
-  /**
-   * Creates or updates a record based on an existing ID.
-   */
-  public save(data: any): Promise<XomApiResponse> {
-    const { Id: id, ...rest } = data
+    if (isNaN(id)) {
+      throw exceptions.missingItemId()
+    }
 
-    return id
-      ? this.update(id, rest)
-      : this.post(rest)
+    return requests.patchListItem(this._http, this._title, id, await this._itemsType, rest)
   }
 
   /**
    * Deletes an existing record from the SharePoint list.
    */
-  public delete(id: number): Promise<XomApiResponse> {
+  public deleteItem(item: any): Promise<XomApiResponse>
+  public deleteItem(itemId: number): Promise<XomApiResponse>
+  public deleteItem(param1: any): Promise<XomApiResponse> {
+    const { Id: id = param1 } = param1
+
+    if (isNaN(id)) {
+      throw exceptions.missingItemId()
+    }
+
     return requests.deleteListItem(this._http, this._title, id)
   }
 
   /**
    * Return a list of the attached files in the list item.
    */
-  public getAttachmentsFrom(itemId: number): Promise<XomApiResponse> {
-    return requests.getListItemAttachments(this._http, this._title, itemId)
+  public getAttachments(item: any): Promise<XomApiResponse>
+  public getAttachments(itemId: number): Promise<XomApiResponse>
+  public getAttachments(param1: any): Promise<XomApiResponse> {
+    const { Id: id = param1 } = param1
+
+    if (isNaN(id)) {
+      throw exceptions.missingItemId()
+    }
+
+    return requests.getListItemAttachments(this._http, this._title, id)
   }
 
   /**
    * Uploads a file attachment to a list item.
    */
-  public async attachTo(
+  public addAttachment(
+    item: any,
+    fileName: string,
+    fileReference: string | HTMLInputElement | FileList | File | ArrayBuffer | Blob,
+  ): Promise<XomApiResponse>
+  public async addAttachment(
     itemId: number,
+    fileName: string,
+    fileReference: string | HTMLInputElement | FileList | File | ArrayBuffer | Blob,
+  ): Promise<XomApiResponse>
+  public async addAttachment(
+    param1: any,
     fileName: string,
     fileReference: string | HTMLInputElement | FileList | File | ArrayBuffer | Blob,
   ): Promise<XomApiResponse> {
     const fileBuffer = await genFileBuffer(fileReference)
+    const { Id: id = param1 } = param1
 
-    return requests.uploadListItemAttachment(this._http, this._title, itemId, fileName, fileBuffer)
+    if (isNaN(id)) {
+      throw exceptions.missingItemId()
+    }
+
+    return requests.uploadListItemAttachment(this._http, this._title, id, fileName, fileBuffer)
   }
 
   /**
    * Renames a given file attachment.
    */
-  public renameAttachment(itemId: number, currentName: string, newName: string): Promise<XomApiResponse> {
-    return requests.renameListItemAttachment(this._http, this._title, itemId, currentName, newName)
+  public renameAttachment(item: any, currentName: string, newName: string): Promise<XomApiResponse>
+  public renameAttachment(itemId: number, currentName: string, newName: string): Promise<XomApiResponse>
+  public renameAttachment(param1: any, currentName: string, newName: string): Promise<XomApiResponse> {
+    const { Id: id = param1 } = param1
+
+    if (isNaN(id)) {
+      throw exceptions.missingItemId()
+    }
+
+    return requests.renameListItemAttachment(this._http, this._title, id, currentName, newName)
   }
 
   /**
    * Removes a given file attachment from the list item.
    */
-  public removeAttachment(itemId: number, fileName: string): Promise<XomApiResponse> {
-    return requests.deleteListItemAttachment(this._http, this._title, itemId, fileName)
+  public deleteAttachment(item: any, fileName: string): Promise<XomApiResponse>
+  public deleteAttachment(itemId: number, fileName: string): Promise<XomApiResponse>
+  public deleteAttachment(param1: any, fileName: string): Promise<XomApiResponse> {
+    const { Id: id = param1 } = param1
+
+    if (isNaN(id)) {
+      throw exceptions.missingItemId()
+    }
+
+    return requests.deleteListItemAttachment(this._http, this._title, id, fileName)
   }
 }
 
