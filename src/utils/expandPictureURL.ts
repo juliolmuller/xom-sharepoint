14f99ea7c3a580085672f39d2a0a5e293fbab1ca
Julio L. Muller <julio.l.muller@exxonmobil.com>
2020-09-30 14:56:24 -0300

    Fiex unsafe nullable value

--- src/utils/expandPictureURL.ts
+++ src/utils/expandPictureURL.ts
@@ -7,14 +7,14 @@ const LARGE_PICTURE_CODE = '_LThumb'
  * Properly expands picture size URL options for given object property.
  */
 function expandPictureURL(userObject: any): any {
-  if (userObject && typeof userObject.Picture !== 'undefined') {
-    const url = userObject.Picture.Url // encodeURI(userObject.Picture.Url)
+  if (userObject) {
+    const url = userObject.Picture?.Url // encodeURI(userObject.Picture.Url)
     const targetExpression = new RegExp(`${SMALL_PICTURE_CODE}|${MEDIUM_PICTURE_CODE}|${LARGE_PICTURE_CODE}`, 'i')
 
     userObject.Picture = {
-      Small: userObject.Picture && url.replace(targetExpression, SMALL_PICTURE_CODE),
-      Medium: userObject.Picture && url.replace(targetExpression, MEDIUM_PICTURE_CODE),
-      Large: userObject.Picture && url.replace(targetExpression, LARGE_PICTURE_CODE),
+      Small: url?.replace(targetExpression, SMALL_PICTURE_CODE) || null,
+      Medium: url?.replace(targetExpression, MEDIUM_PICTURE_CODE) || null,
+      Large: url?.replace(targetExpression, LARGE_PICTURE_CODE) || null,
     }
   }
 
